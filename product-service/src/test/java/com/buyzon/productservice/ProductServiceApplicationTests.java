85508343857a0cfa9b838e355986dca8b2488aaf
Ambuj Rajput <ambraj@gmail.com>
2021-09-14 16:37:23 +0530

    adding product service project - incomplete

---
+++ product-service/src/test/java/com/buyzon/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyzon.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
